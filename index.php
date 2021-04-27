0f94b873120f43614e4c8cca8cc79bffbf36eeed
simranpasrija <69893107+simranpasrija@users.noreply.github.com>
2021-04-28 00:19:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>online voting</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<!-- <link href="css/user_styles.css" rel="stylesheet" type="text/css" /> -->
+<script language="JavaScript" src="js/user.js">
+</script>
+
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-pinterest" href="https://uk.pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+        <li><a class="faicon-twitter" href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+        <li><a class="faicon-google-plus" href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a></li>
+        <li><a class="faicon-rss" href="https://www.rss.com/"><i class="fa fa-rss"></i></a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace inline pushright">
+        <li><i class="fa fa-phone"></i> +91-8387318835</li>
+        <li><i class="fa fa-envelope-o"></i> admin@gmail.com </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">ONLINE VOTING</a></h1>
+    </div>
+    <!-- ################################################################################################ -->
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a class="drop" href="#">Pages</a>
+          <ul>
+            <li><a href="admin/index.php">Admin</a></li>
+            <li><a href="login.php">Voter</a></li>
+            
+          </ul>
+        </li>
+        <li><a class="drop" href="#">Voter Panel</a>
+          <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registeracc.php">Registration</a></li>
+            
+          </ul>
+        </li>
+       
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/Online-Ballots.png');">
+  <section id="testimonials" class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <h2 class="font-x3 uppercase btmspace-80 underlined"> Online <a href="#">Voting</a></h2>
+    <ul class="nospace group">
+      <li class="one_half">
+        <blockquote>An Online Election system is an efficient and convenient alternative to the hectic process of in person voting. Such a software with a fully functional and secure database proves to be an extremely useful alternative/ replacement to a lengthy and tiring process that has been carried on for far too long despite the existence of complete technological developments.</blockquote>
+      
+      </li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_third first">
+      <h6 class="title">Address</h6>
+      <ul class="nospace linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+         
+          <p>
+          Name        : Shubham Sahu <br>
+          University  : DTU <br>
+          Batch       : 2K19 <br>
+          Dept        : SE <br>
+          </p>
+          </address>
+        </li>
+      </ul>
+    </div>
+
+    <div class="one_third">
+      <h6 class="title">Phone</h6>
+      <ul class="nospace linklist contact">
+       
+        <li><i class="fa fa-phone"></i> +91-8387318835<br>
+          +91-9672815381</li>
+
+
+      </ul>
+    </div>
+
+    <div class="one_third">
+      <h6 class="title">Email</h6>
+      <ul class="nospace linklist contact">
+        
+        <li><i class="fa fa-envelope-o"></i> admin@gmail.com </li>
+
+      </ul>
+    </div>
+
+
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="#">Shubham Sahu</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+</body>
+</html>
+
+
